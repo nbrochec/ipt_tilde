34ae909ca39d041adb1b1fdefa7bc2bd87be46c3
jobor019 <joakimborg@hotmail.com>
2024-09-26 12:46:15 +0200

    Added leaky integration and a number of attributes

--- ipt_tilde/ipt_tilde.cpp
+++ ipt_tilde/ipt_tilde.cpp
@@ -1,6 +1,7 @@
 #include "c74_min.h"
 #include <torch/script.h>
 #include <torch/torch.h>
+#include <chrono>
 
 using namespace c74::min;
 
@@ -14,15 +15,11 @@ struct ClassificationResult {
 class IptClassifier {
 public:
     explicit IptClassifier(const std::string& path) {
-        try {
-            m_buffer = std::vector<float>(m_size, 0.0f);
-            at::init_num_threads();
-            m_model = torch::jit::load(path);
-            m_model.eval();
-            m_model.to(torch::kCPU);
-        } catch (c10::Error& e) {
-            throw std::runtime_error(e.what());
-        }
+        m_buffer = std::vector<float>(m_size, 0.0f);
+        at::init_num_threads();
+        m_model = torch::jit::load(path);
+        m_model.eval();
+        m_model.to(torch::kCPU);
     }
 
     std::optional<ClassificationResult> process(std::vector<double>&& input) {
@@ -50,7 +47,7 @@ public:
         return ClassificationResult{v, amax};
     }
 
-private:
+
     static std::size_t argmax(const std::vector<float>& v) {
         float max = v[0];
         std::size_t index = 0;
@@ -63,6 +60,8 @@ private:
         return index;
     }
 
+
+private:
     template<typename T>
     static std::vector<T> tensor2vector(const at::Tensor& tensor) {
         std::vector<float> v;
@@ -90,6 +89,59 @@ private:
 
 // ==============================================================================================
 
+class LeakyIntegrator {
+public:
+    std::vector<float> process(const std::vector<float>& input) {
+        auto current_time = std::chrono::system_clock::now();
+
+        if (!m_last_callback || m_tau < 1e-6 || m_previous_value.size() != input.size()) {
+            m_last_callback = current_time;
+            m_previous_value = input;
+            return input;
+        }
+
+        auto elapsed_time = static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(
+                current_time - *m_last_callback).count());
+
+        elapsed_time = std::min(m_tau, std::max(0.0, elapsed_time));
+
+        auto output = integrate(input, elapsed_time);
+        m_previous_value = output;
+        m_last_callback = current_time;
+
+        return output;
+    }
+
+    void set_tau(double tau) {
+        m_tau = tau;
+    }
+
+
+private:
+    std::vector<float> integrate(const std::vector<float>& current_value, double elapsed_time) {
+        std::vector<float> result;
+        result.reserve(current_value.size());
+
+        auto dt = elapsed_time / m_tau;
+
+        for (std::size_t i = 0; i < current_value.size(); i++) {
+            result.emplace_back((1 - dt) * m_previous_value.at(i) + dt * current_value.at(i));
+        }
+        return result;
+    }
+
+
+    std::optional<std::chrono::time_point<std::chrono::system_clock>> m_last_callback;
+    std::vector<float> m_previous_value;
+
+    double m_tau = 0.0;
+
+
+};
+
+
+// ==============================================================================================
+
 class ipt_tilde : public object<ipt_tilde>, public vector_operator<> {
 private:
     std::thread m_processing_thread;
@@ -97,7 +149,10 @@ private:
     c74::min::fifo<ClassificationResult> m_event_fifo{100};
 
     std::atomic<bool> m_running = false;
+    std::atomic<bool> m_enabled = true;
     std::string m_path;
+
+    LeakyIntegrator m_integrator;
 
 public:
     MIN_DESCRIPTION{""};                   // TODO
@@ -112,10 +167,22 @@ public:
     outlet<> dumpout{this, "(any) dumpout"};
 
     explicit ipt_tilde(const atoms& args = {}) {
+
         if (!args.empty() && args[0].type() == c74::min::message_type::symbol_argument) {
-            m_path = static_cast<std::string>(args[0]);
-            m_running = true;
-            m_processing_thread = std::thread([&]() { this->process(); });
+            auto path = std::string(args[0]);
+
+            if (path.substr(path.length() - 3) != ".ts") {
+                path = path + ".ts";
+            }
+
+            try {
+                m_path = static_cast<std::string>(c74::min::path(path));
+                m_running = true;
+                m_processing_thread = std::thread([&]() { this->process(); });
+            } catch (std::runtime_error& e) {
+                // failed to locate file
+                error(e.what());
+            }
 
         } else {
             error("Missing argument: filepath to model");
@@ -134,17 +201,19 @@ public:
             this, MIN_FUNCTION {
                 ClassificationResult result;
                 while (m_event_fifo.try_dequeue(result)) {
-                    atoms distribution;
-                    for (std::size_t i = 0; i < result.distribution.size(); ++i) {
-                        distribution.emplace_back(result.distribution[i]);
+                    auto distribution = m_integrator.process(result.distribution);
+
+                    atoms distribution_atms;
+
+                    for (const auto& v : distribution) {
+                        distribution_atms.emplace_back(v);
                     }
 
                     atoms argmax;
-                    argmax.emplace_back(result.argmax);
-
+                    argmax.emplace_back(IptClassifier::argmax(distribution));
 
                     outlet_main.send(argmax);
-                    outlet_distribution.send(distribution);
+                    outlet_distribution.send(distribution_atms);
                 }
                 return {};
             }
@@ -152,7 +221,7 @@ public:
 
 
     void operator()(audio_bundle in, audio_bundle) override {
-        if (in.channel_count() > 0 && m_running) {
+        if (in.channel_count() > 0 && m_running && m_enabled) {
             for (auto i = 0; i < in.frame_count(); ++i) {
                 m_audio_fifo.try_enqueue(in.samples(0)[i]);
             }
@@ -160,7 +229,49 @@ public:
     }
 
 
-    attribute<bool> verbose{ this, "verbose", false};
+    attribute<bool> verbose{this, "verbose", false};
+
+    attribute<bool> enabled{this, "enabled", true, setter{
+            MIN_FUNCTION {
+                if (args[0].type() == c74::min::message_type::int_argument) {
+                    m_enabled = static_cast<bool>(args[0]);
+                    return enabled;
+                }
+
+                cerr << "bad argument for message \"enabled\"" << endl;
+                return enabled;
+            }
+    }
+    };
+
+    attribute<double> sensitivity{this, "sensitivity", 1.0, setter{
+            MIN_FUNCTION {
+                if (args.size() == 1 && args[0].type() == c74::min::message_type::float_argument) {
+                    auto tau = std::min(1.0, std::max(0.0, static_cast<double>(args[0])));
+                    m_integrator.set_tau((1.0-tau) * static_cast<double>(sensitivityrange.get()));
+                    return {tau};
+                }
+
+                cerr << "bad argument for message \"sensitivity\"" << endl;
+                return sensitivity;
+            }
+    }
+    };
+
+    attribute<int> sensitivityrange{this, "sensitivityrange", 2000, setter{
+            MIN_FUNCTION {
+                if (args.size() == 1
+                    && args[0].type() == c74::min::message_type::int_argument
+                    && static_cast<int>(args[0]) > 0) {
+                    m_integrator.set_tau(sensitivity.get() * (1.0 - static_cast<double>(args[0])));
+                    return args;
+                }
+
+                cerr << "bad argument for message \"sensitivityrange\"" << endl;
+                return sensitivityrange;
+            }
+    }
+    };
 
 
     // TODO: Dummy maxclass_setup. Remove or replace
@@ -172,39 +283,52 @@ public:
         return {};
     }};
 
+
 private:
-
     void process() {
+        std::unique_ptr<IptClassifier> classifier;
+
         try {
-            IptClassifier classifier{m_path};
-
-            while (m_running) {
-                std::vector<double> buffered_audio;
-                double sample;
-                while (m_audio_fifo.try_dequeue(sample)) {
-                    buffered_audio.push_back(sample);
-                }
-
-                if (!buffered_audio.empty()) {
-                    auto label = classifier.process(std::move(buffered_audio));
-                    if (label) {
-                        m_event_fifo.try_enqueue(*label);
-                        deliverer.delay(0.0);
-                    }
-                }
-
-                std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            }
-        } catch (std::runtime_error& e) {
+            classifier = std::make_unique<IptClassifier>(m_path);
+        } catch (c10::Error& e) {
             if (verbose.get()) {
                 cerr << e.what() << endl;
             } else {
                 cerr << "error during loading" << endl;
             }
+            return;
+        }
+
+        try {
+            while (m_running) {
+                if (m_enabled) {
+                    std::vector<double> buffered_audio;
+                    double sample;
+                    while (m_audio_fifo.try_dequeue(sample)) {
+                        buffered_audio.push_back(sample);
+                    }
+
+                    if (!buffered_audio.empty()) {
+                        auto label = classifier->process(std::move(buffered_audio));
+                        if (label) {
+                            m_event_fifo.try_enqueue(*label);
+                            deliverer.delay(0.0);
+                        }
+                    }
+                }
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+
+        } catch (c10::Error& e) {
+            if (verbose.get()) {
+                cerr << e.what() << endl;
+            } else {
+                cerr << "model architecture is not compatible" << endl;
+            }
         }
 
     }
-
 
 };
 
