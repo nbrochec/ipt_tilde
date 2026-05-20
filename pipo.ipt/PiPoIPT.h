6fc46ee6a471560cae4be12deacb40f90a6dabfb
Diemo Schwarz <schwarz@ircam.fr>
2026-05-20 19:43:57 +0200

    spaces and style

--- pipo.ipt/PiPoIPT.h
+++ pipo.ipt/PiPoIPT.h
@@ -1,3 +1,4 @@
+// -*- mode: c++; c-basic-offset:2 -*-
 
 #include "PiPo.h"
 #include "ipt_classifier.h"
@@ -6,7 +7,7 @@ class PiPoIPT : public PiPo
 {
 private:
   std::unique_ptr<IptClassifier> classifier_;
-  std::vector<double> inputbuf;
+  std::vector<double> inputbuf_;
 
 public:
   PiPoScalarAttr<const char *>    modelname_attr_;
@@ -19,13 +20,13 @@ public:
 
   PiPoIPT (Parent *parent, PiPo *receiver = NULL)
   : PiPo(parent, receiver),
-  modelname_attr_	       (this, "model", "Absolute path to model.ts", true, ""),
-  device_attr_		       (this, "device", "Device type", true, 0),
-  sensitivity_attr_	     (this, "sensitivity", "Adjust the sensitivity of classification output", false, 0.5),
-  sensitivityrange_attr_ (this, "sensitivityrange", "Set the time window for sensitivity scaling", false, 500),
-  threshold_attr_	       (this, "threshold", "Set the energy threshold for classification", false, -80),
-  window_attr_	         (this, "window", "Set the sliding window size for energy thresholding", false, 20),
-  confidence_attr_	     (this, "confidence", "Set the minimum confidence threshold for classification output", false, 0.2)
+    modelname_attr_            (this, "model", "Absolute path to model.ts", true, ""),
+    device_attr_               (this, "device", "Device type", true, 0),
+    sensitivity_attr_          (this, "sensitivity", "Adjust the sensitivity of classification output", false, 0.5),
+    sensitivityrange_attr_     (this, "sensitivityrange", "Set the time window for sensitivity scaling", false, 500),
+    threshold_attr_            (this, "threshold", "Set the energy threshold for classification", false, -80),
+    window_attr_               (this, "window", "Set the sliding window size for energy thresholding", false, 20),
+    confidence_attr_           (this, "confidence", "Set the minimum confidence threshold for classification output", false, 0.2)
   {
     device_attr_.addEnumItem("CPU",  "Use CPU");
     device_attr_.addEnumItem("CUDA", "NVIDIA GPU");
@@ -51,17 +52,17 @@ public:
     switch (device_attr_.get())
     {
       case 1: device = torch::kCUDA;    break;
-      case 2: device = torch::kMPS;	    break;
+      case 2: device = torch::kMPS;         break;
       default: device = torch::kCPU;    break;
     }
 
     if (model_path  &&  model_path[0] != 0)
     {
       try {
-      	classifier_ = std::make_unique<IptClassifier>(std::string(model_path), device);
-      	classifier_->initialize_model();
-      	classifier_->initialize_buffers(sr, maxFrames);
-        inputbuf.reserve(maxFrames);
+        classifier_ = std::make_unique<IptClassifier>(std::string(model_path), device);
+        classifier_->initialize_model();
+        classifier_->initialize_buffers(sr, maxFrames);
+        inputbuf_.reserve(maxFrames);
       }
       catch (std::runtime_error& e)
       {
@@ -76,12 +77,12 @@ public:
 
       const char **out_labels = new const char *[numclasses];
       for (int i = 0; i < numclasses; i++)
-	    out_labels[i] = classnames[i].c_str();
+            out_labels[i] = classnames[i].c_str();
 
       // determine output stream parameters
       double out_framerate = sr / maxFrames;
       ret = propagateStreamAttributes(true, out_framerate, 0, numclasses, 1,
-					  out_labels, false, 0.001 / out_framerate, 1);
+                                          out_labels, false, 0.001 / out_framerate, 1);
       delete [] out_labels;
     }
     
@@ -95,14 +96,14 @@ public:
     if (classifier_)
     {
       if (size == 1)
-        inputbuf.assign(values, values + num); // copy to required double vector
+        inputbuf_.assign(values, values + num); // copy to required double vector
       else // copy to required double vector and reduce to mono
       {
-        inputbuf.assign(num, 0); // set to num zeros
+        inputbuf_.assign(num, 0); // set to num zeros
         for (int i = 0; i < num; i++)
         {
           for (int j = 0; j < size; j++)
-            inputbuf[i] += values[j];
+            inputbuf_[i] += values[j];
 
           values += size;
         }
@@ -110,7 +111,7 @@ public:
 
       std::optional<ClassificationResult> result;
       try {
-        result = classifier_->process(std::move(inputbuf));
+        result = classifier_->process(std::move(inputbuf_));
       }
       catch (std::runtime_error& e)
       {
@@ -132,5 +133,3 @@ public:
     return ret;
   }
 };
-
-
